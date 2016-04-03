efec9c6ff9426939e51b2a4d4313f8a4ec1004f2
Vincent <vcapic@gmail.com>
2016-04-03 22:09:20 +0200

    changement de la table session

--- utils/bdd.py
+++ utils/bdd.py
@@ -3,7 +3,7 @@ from sqlalchemy import *
 from sqlalchemy.orm import (scoped_session, sessionmaker, relationship, backref)
 from sqlalchemy.ext.declarative import declarative_base
 import config
-from beans.session import Session
+# from beans.session import Session
 
 
 engine = create_engine(config.BDD_STRING_CONNECTION, convert_unicode=True)
@@ -14,12 +14,12 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 Base = declarative_base()
 
 
-def start_track_session():
-    session = Session()
-
-    # db_session.query(Session).filter()
-
-    return session
+# def start_track_session():
+#     session = Session()
+#
+#     # db_session.query(Session).filter()
+#
+#     return session
 
 # Base.query = db_session.query_property()
 # Base.metadata.create_all(engine)
