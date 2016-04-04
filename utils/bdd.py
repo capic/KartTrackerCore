6b15883b53f6921bb656a81fd9d9073682bf774c
Vincent <vcapic@gmail.com>
2016-04-04 12:46:48 +0200

    changement de la table session

--- utils/bdd.py
+++ utils/bdd.py
@@ -3,7 +3,7 @@ from sqlalchemy import *
 from sqlalchemy.orm import (scoped_session, sessionmaker, relationship, backref)
 from sqlalchemy.ext.declarative import declarative_base
 import config
-from beans.session import session
+from beans.session import Session
 from datetime import date
 
 
@@ -16,7 +16,7 @@ Base = declarative_base()
 
 
 def start_track_session(track_id):
-    track_session = session.Session()
+    track_session = Session()
 
     qry = db_session.query(func.max(Session.id_day_session).label("max_id_day_session")).filter(Session.date_session == date.today()).filter(Session.track_id == track_id)
     res = qry.one()
