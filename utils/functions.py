e1b13550b7322df3e7b7df8139a4d0c6d6792e4d
Vincent <vcapic@gmail.com>
2016-04-11 07:37:19 +0200

    correction des variables

--- utils/functions.py
+++ utils/functions.py
@@ -11,11 +11,11 @@ def is_interface_up(interface):
 
 def flash_led(stop):
     while not stop:
-        GPIO.output(config.PIN_NUMBER_RUNING, False)
+        GPIO.output(config.PIN_NUMBER_LED, False)
         time.sleep(0.5)
-        GPIO.output(config.PIN_NUMBER_RUNING, True)
+        GPIO.output(config.PIN_NUMBER_LED, True)
         time.sleep(0.5)
 
 
 def led_on():
-    GPIO.output(config.PIN_NUMBER_RUNING, True)
+    GPIO.output(config.PIN_NUMBER_LED, True)
