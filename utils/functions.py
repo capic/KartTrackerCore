ec90899af0bc8c3c90a9ba1bd40a9e2e2e20b02b
Vincent <vcapic@gmail.com>
2016-04-12 07:41:20 +0200

    changement dans l'execution du thread

--- utils/functions.py
+++ utils/functions.py
@@ -3,6 +3,7 @@ import netifaces
 import RPi.GPIO as GPIO
 import time
 import utils.config as config
+import utils.log as log
 
 
 def is_interface_up(interface):
@@ -11,6 +12,8 @@ def is_interface_up(interface):
 
 
 def flash_led(stop_blinking):
+    log.log("flash_led", log.LEVEL_INFO)
+
     while not stop_blinking.isSet():
         print('led off')
         GPIO.output(config.PIN_NUMBER_LED, False)
@@ -19,7 +22,9 @@ def flash_led(stop_blinking):
         GPIO.output(config.PIN_NUMBER_LED, True)
         time.sleep(0.5)
 
+    log.log("before wait", log.LEVEL_DEBUG)
     stop_blinking.wait()
+    log.log("after wait", log.LEVEL_DEBUG)
 
 
 def led_on():
