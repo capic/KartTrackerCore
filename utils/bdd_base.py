493c67a4c39c3c1fe3d5a6b43df0ae0165879bb8
Vincent <vcapic@gmail.com>
2016-04-13 22:18:17 +0200

    traitement de l'envoi en central

--- utils/bdd_base.py
+++ utils/bdd_base.py
@@ -50,7 +50,7 @@ class Session(Base):
     track_id = Column(Integer, ForeignKey('track.id'))
 
     def __repr__(self):
-        return '{id: ' + str(self.id) + ', id_day_session: ' + str(self.id_day_session) + ', date_session: ' + str(
+        return '<Session>:{id: ' + str(self.id) + ', id_day_session: ' + str(self.id_day_session) + ', date_session: ' + str(
             self.date_session) + ', name: ' + str(self.name) + ', start_time: ' + str(
             self.start_time) + ', end_time: ' + str(self.end_time) + ', track_id: ' + str(
             self.track_id) + '}'
