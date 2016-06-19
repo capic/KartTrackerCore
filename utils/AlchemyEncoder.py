ef57e6a90407b844a8ff55c3d7064b98a364e233
Vincent <vcapic@gmail.com>
2016-06-19 20:41:47 +0200

    changement de la facon d'uploader a cause de la limitation du nombre de donnees qui peuvent etre envoyés

--- utils/AlchemyEncoder.py
+++ utils/AlchemyEncoder.py
@@ -29,7 +29,8 @@ def new_alchemy_encoder(revisit_self=False, fields_to_expand=[], nested_object=T
                         # unless we're expanding this field, stop here
                         if field not in fields_to_expand:
                             # not expanding this field: set it to None and continue
-                            fields[field] = None
+                            if nested_object:
+                                fields[field] = None
                             continue
 
                     # fields[field] = val
